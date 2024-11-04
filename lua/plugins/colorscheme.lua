fc257ae995202d5c0e4e413b0060e2da44465c34
andrew_dao <hoandao.dev@gmail.com>
2024-11-04 14:27:08 +0700

    mm

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,21 @@
 return {
+  -- {
+  --   "datsfilipe/min-theme.nvim",
+  --   config = function()
+  --     require("min-theme").setup({
+  --       theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
+  --       transparent = false, -- Boolean: Sets the background to transparent
+  --       italics = {
+  --         comments = true, -- Boolean: Italicizes comments
+  --         keywords = true, -- Boolean: Italicizes keywords
+  --         functions = true, -- Boolean: Italicizes functions
+  --         strings = true, -- Boolean: Italicizes strings
+  --         variables = true, -- Boolean: Italicizes variables
+  --       },
+  --       overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+  --     })
+  --   end,
+  -- },
   {
     "projekt0n/github-nvim-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -6,7 +23,7 @@ return {
     config = function()
       require("github-theme").setup({
         options = {
-          -- transparent = true,
+          transparent = true,
           terminal_colors = false,
           styles = { -- Style to be applied to different syntax groups
             comments = "italic", -- Value is any valid attr-list value `:help attr-list`
